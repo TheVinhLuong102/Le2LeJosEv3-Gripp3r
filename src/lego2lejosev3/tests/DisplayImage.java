e04571d28783a3bc9f3d0d5404dce0e3d3205997
rolblo12 <rolblo12@gmail.com>
2018-12-14 09:50:10 +0100

    completed Display class;

--- src/lego2lejosev3/tests/DisplayImage.java
+++ src/lego2lejosev3/tests/DisplayImage.java
@@ -3,11 +3,13 @@
  */
 package lego2lejosev3.tests;
 
+import java.io.File;
+import java.io.FilenameFilter;
 import java.util.logging.Logger;
 
 import lego2lejosev3.logging.Setup;
 import lego2lejosev3.pblocks.Display;
-import lego2lejosev3.pblocks.Utl;
+import lejos.hardware.Button;
 
 /**
  * Test for playing a file.
@@ -19,9 +21,6 @@ public class DisplayImage {
 	private static Class<?> clazz = DisplayImage.class;
 	private static final Logger log = Logger.getLogger(clazz.getName());
 
-	// sound files to play
-	private static final String[] imageFiles = new String[] { "IR beacon.bmp" };
-
 	/**
 	 * Main program entry point.
 	 * 
@@ -31,15 +30,31 @@ public class DisplayImage {
 		// setup logging to file
 		Setup.log2File(clazz);
 		log.fine("Starting ...");
+		// determine which image files are there
+		File imgDir = new File(Display.IMAGE_DIR);
+		String[] imageFiles = imgDir.list(new FilenameFilter() {
+			@Override
+			public boolean accept(File dir, String name) {
+				log.fine("found file " + dir.getAbsolutePath() + File.separator + name);
+				return name.toLowerCase().endsWith(".lni");
+			}
+		});
+		if (imageFiles == null || imageFiles.length == 0) {
+			log.fine("No image files found");
 
-		for (String imageFile : imageFiles) {
-			log.fine("Displaying " + imageFile);
-			// display image file on top left corner
-			Display.image(imageFile, true, 0, 0);
-			// Wait 10 seconds
-			Utl.waitTime(10F);
+		} else {
+
+			for (String imageFile : imageFiles) {
+
+				// display image file on top left corner
+				log.fine("Display image " + imageFile);
+				Display.image(imageFile, true, 0, 0);
+				log.fine("Display image done");
+
+				// Wait until button press
+				Button.waitForAnyPress();
+			}
 		}
-
 		log.fine("The End");
 	}
 
